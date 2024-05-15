e1600d7d20bc4b8708796ab5cff625946d9091f8
Hugo Ramirez <hugo.ramirez7857@alumnos.udg.mx>
2024-05-15 15:59:52 -0600

    Arreglar nltk

--- NGrama.py
+++ NGrama.py
@@ -1,7 +1,21 @@
-import nltk.corpus 
+import nltk.corpus
 
 sentences = nltk.corpus.brown.sentences()
 
 len(sentences)
 
 sentences[0]
+
+n_grams = {}
+
+for sentence in sentences:
+    words = [words for word in sentence if word[0].isalpha()]
+
+    for ix in range(len(words)-1):
+        try:
+            n_grams[words[ix]].append(words[ix+1])
+        except KeyError as _:
+            n_grams[words[ix]] = []
+            n_grams[words[ix]].append(words[ix+1])
+
+        n_grams
